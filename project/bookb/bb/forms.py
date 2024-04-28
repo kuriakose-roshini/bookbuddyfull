c9528c2f6ce79b07ce0da3b38f72d985b56f906f
Soorya <pradeepsoorya73@gmail.com>
2024-04-28 15:57:24 +0530

    update

---
+++ project/bookb/bb/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+#class RegisterForm(UserCreationForm):
+ #   name = forms .CharField(max_length=50,required=False)
+
+  #  class Meta:
+    #    model = User
+     #   fields = ["name","idno","emailid","u_name","pwd1","pwd2"]
